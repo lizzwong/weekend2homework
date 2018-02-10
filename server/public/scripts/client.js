bc6ec265f79513ac0944f7faeb1272ceae3e0537
Elizabeth Wong <lizzwong@gmail.com>
2018-02-10 14:14:39 -0600

    yay more structure...i guess

---
+++ server/public/scripts/client.js
@@ -0,0 +1,22 @@
+$(document).ready(onReady);
+
+function onReady() {
+   
+    
+}
+
+function getEquation() {
+    let xValue = $('#x').val();
+    let arithmetics = $('#mathType').val();
+    let yValue = $('#y').val();
+        let equation = {
+            xValue = xValue,
+            arithmetic = arithmetic,
+            yValue = yValue
+        }
+    return equasion;
+}
+
+function addEquasion (){
+    let newEquasion = getEquasion
+}
--- server/server.js
+++ server/server.js
@@ -4,4 +4,16 @@ const app = express();
 app.use(express.static('server/public'));
 
 const bodyParser = require('body-parser');
-app.use( bodyParser.urlencoded( {extend: true_} ));
+app.use( bodyParser.urlencoded( {extend: true } ));
+
+// const mathRouter = require('./router/mathRouter');
+// app.use('/maths',mathRouter);
+
+
+
+
+const port = 8888;
+app.listen(port,function(){
+    console.log(`Listening on port ${port}`);
+    
+});
